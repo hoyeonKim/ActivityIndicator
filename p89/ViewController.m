d436023bfb7780605b82674ca7ec1d7503c6b2ed
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-03 15:46:20 +0900

    ActivityIndicator
    완료했습니다.

--- p89/ViewController.m
+++ p89/ViewController.m
@@ -9,14 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator1;
+@property (strong,nonatomic) UIActivityIndicatorView *indicator2;
 @end
 
 @implementation ViewController
-
+-(IBAction)startIndicator:(id)sender{
+    [_indicator1 startAnimating];
+    [_indicator2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];}
+-(IBAction)stopIndicator:(id)sender{
+    [_indicator1 stopAnimating];
+    [_indicator2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _indicator2 = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _indicator2.hidesWhenStopped = YES;
+    _indicator2.center= CGPointMake(160, 300);
+    [self.view addSubview:_indicator2];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
